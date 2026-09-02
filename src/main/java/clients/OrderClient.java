816288ca6242882ba77abb39eceae80c484384b6
Eva Konovalova <eva12092002@yandex.ru>
2026-09-02 16:59:05 +0300

    Tests to verify the order create request

---
+++ src/main/java/clients/OrderClient.java
@@ -0,0 +1,23 @@
+package clients;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import models.Order;
+
+import static io.restassured.RestAssured.given;
+
+public class OrderClient {
+
+    private static final String BASE_URI = "https://qa-scooter.praktikum-services.ru";
+    private static final String ORDER_PATH = "/api/v1/orders";
+
+    public Response createOrder(Order order) {
+        return given()
+                .filter(new AllureRestAssured())
+                .contentType(ContentType.JSON)
+                .body(order)
+                .when()
+                .post(BASE_URI + ORDER_PATH);
+    }
+}
