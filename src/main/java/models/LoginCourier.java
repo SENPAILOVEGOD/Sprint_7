57f4de571741fd7630d72d34c8a6686cb0cf454b
Eva Konovalova <eva12092002@yandex.ru>
2026-09-05 12:50:48 +0300

    Using Lombok for code generation

--- src/main/java/models/LoginCourier.java
+++ src/main/java/models/LoginCourier.java
@@ -1,32 +1,16 @@
 package models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginCourier {
 
     private String login;
     private String password;
-
-    public LoginCourier(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
-
-    public LoginCourier() {
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
